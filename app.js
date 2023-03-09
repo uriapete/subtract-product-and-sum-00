806bfc2ca0dc516b2b2cc998f1e6efb074f9b84c
uriapete <uriarteangelo@gmail.com>
2023-03-08 16:46:54 -0800

    Created funct code, appears to be working in first two test cases

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
 "use strict";
 function subtractProductAndSum(n) {
-    return n;
+    let result = 0;
+    // turn n into string
+    // iterate through string, we don't need to modify!
+    // push all digits to an array
+    // multiply all items, save that
+    // sum up all items, save that
+    // subtract, save to result
+    // return result
+    const digiArr = [];
+    for (let i = 0; i < n.toString().length; i++) {
+        const element = n.toString()[i];
+        digiArr.push(parseInt(element));
+    }
+    let product = 1;
+    let sum = 0;
+    for (let i = 0; i < digiArr.length; i++) {
+        const element = digiArr[i];
+        product *= element;
+    }
+    for (let i = 0; i < digiArr.length; i++) {
+        const element = digiArr[i];
+        sum += element;
+    }
+    result = product - sum;
+    return result;
 }
 ;
-const o = 1;
+console.log(subtractProductAndSum(234));
+console.log(subtractProductAndSum(4421));
--- app.ts
+++ app.ts
@@ -1,3 +1,39 @@
 function subtractProductAndSum(n: number): number {
-    return n;
+    
+    let result: number = 0;
+
+    // turn n into string
+    // iterate through string, we don't need to modify!
+    // push all digits to an array
+    // multiply all items, save that
+    // sum up all items, save that
+    // subtract, save to result
+    // return result
+
+    const digiArr: Array<number> = [];
+
+    for (let i = 0; i < n.toString().length; i++) {
+        const element = n.toString()[i];
+        digiArr.push(parseInt(element));
+    }
+
+    let product: number = 1;
+    let sum: number = 0;
+
+    for (let i = 0; i < digiArr.length; i++) {
+        const element = digiArr[i];
+        product *= element;
+    }
+
+    for (let i = 0; i < digiArr.length; i++) {
+        const element = digiArr[i];
+        sum += element
+    }
+
+    result = product - sum;
+
+    return result;
 };
+
+console.log(subtractProductAndSum(234));
+console.log(subtractProductAndSum(4421));
