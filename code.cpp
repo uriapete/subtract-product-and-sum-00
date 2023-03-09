05df8821d7e24420fd1c4504146a384698fae0b8
uriapete <uriarteangelo@gmail.com>
2023-03-08 17:03:29 -0800

    Created code for function in cpp, ready to test

--- code.cpp
+++ code.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <string>
+#include <array>
+#include <vector>
 
-class Solution {
+class Solution
+{
 public:
-    int subtractProductAndSum(int n) {
+    int subtractProductAndSum(int n)
+    {
 
         // pseudocode time
 
@@ -15,7 +19,44 @@ public:
         // subtract, save to result
         // return result
 
-        
+        // init return val
+        int result = 0;
 
+        std::string nStr = std::to_string(n);
+
+        // init digi arr
+        std::vector<int> digiVect = {};
+
+        // pushing digits into array
+        for (int16_t i = 0; i < nStr.length(); i++)
+        {
+            digiVect.push_back(nStr[i]);
+        }
+
+        // initting sum and product vars
+        int sum = 0;
+        int product = 1;
+
+        // calculating product of all digits:
+        for (int16_t i = 0; i < digiVect.size(); i++)
+        {
+            product *= digiVect[i];
+        }
+
+        // calculating sum of all digits:
+        for (int16_t i = 0; i < digiVect.size(); i++)
+        {
+            sum += digiVect[i];
+        }
+
+        // difference of prod - sum
+        result = product - sum;
+
+        // ready to return
+        return result;
     }
 };
+
+int main()
+{
+}
