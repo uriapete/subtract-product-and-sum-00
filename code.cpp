8bdb56ae78e4d4f81f68d316520803b3d63e1a5b
uriapete <uriarteangelo@gmail.com>
2023-03-08 16:53:48 -0800

    Included include statements in cpp file

---
+++ code.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+class Solution {
+public:
+    int subtractProductAndSum(int n) {
+
+        // pseudocode time
+
+        // turn n into string
+        // iterate through string, we don't need to modify!
+        // push all digits to an array
+        // multiply all items, save that
+        // sum up all items, save that
+        // subtract, save to result
+        // return result
+
+        
+
+    }
+};
